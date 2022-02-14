6e7fb8fcc45106df83384d8490d295a01a3abe6b
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-02-14 19:57:18 +0000

    Add welcome screen message and input for player username

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
+import random
 from words import words
 
-print(words)
+def welcome_screen():
+    """
+    This is the home screen and will take the players username
+    and welcome them to the game.
+    """
+    player_name = input("Please enter your username:\n ")
+    print(f"Hi {player_name}, welcome to The Hangman's stage. Please try to guess what The Hangman's word is and see if you can escape him")
+
+welcome_screen()
