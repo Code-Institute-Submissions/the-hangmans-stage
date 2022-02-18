253e060daf65cc472ad5d8a8ef16732bf92040e7
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-02-18 17:02:07 +0000

    Add theme selection function

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import random
-from words import words
+from words import superheroes
 
 def welcome_screen():
     """
@@ -18,25 +18,35 @@ def welcome_screen():
     while True:
         play_game = input("Would you like to play Hangman? Enter 'y' for yes and 'n' for no: \n ").lower()
         if play_game == "y":
-            run_game()
+            get_theme()
         elif play_game == "n":
             end_game()
         else:
             print("Please enter a valid selection")
 
+def get_theme():
+    while True:
+        print("Please choose a theme of secret words")
+        print("1 - Superheroes")
+        print("2 - Cartoons")
+        theme = input(" \n")
+        if theme == "1":
+            return superheroes
+            run_game()
+
 def get_word(choice):
     """
     Will randomly select a word from words.py for the player to guess.
     """
-    word = random.choice(words)
+    word = random.choice(word)
     return word
 
 
-def run_game():
+def run_game(word):
     """
     Will start the game if the user enters 'y' on the welcome screen.
     """
-    word = get_word(words)
+    word = get_word(get_theme())
     secret = "_" * len(word)
     print(f" guess the word: {secret}")
     print(word)
